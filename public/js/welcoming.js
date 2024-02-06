36a3860d6ab6103d591f7b526250752ee49da93e
Muh Zaidan Fauzan <118703780+Zaidanfzn@users.noreply.github.com>
2024-02-06 11:51:42 +0700

    Added functional effects in welcoming.js
    Added code for the 2024 Dlor description effect function used in homepage.html
    
    Signed-off-by: Muh Zaidan Fauzan <118703780+Zaidanfzn@users.noreply.github.com>

--- public/js/welcoming.js
+++ public/js/welcoming.js
@@ -11,3 +11,44 @@ function hide() {
 function toggleModal() { 
     document.getElementById('modal').classList.toggle('hidden')
 }
+
+function startAnimation() {
+  anime.timeline({loop: false})
+  .add({
+    targets: '.h3-part1, .h3-part2, .stamp',
+    scale: [7,1],
+    opacity: [0,1],
+    easing: "easeOutCirc",
+    duration: 900,
+    delay: (el, i) => 1600 * i
+  })
+  .add({
+    targets: '#h3-text, #stamp',
+    opacity: 1,
+    duration: 900,
+    easing: "easeOutExpo",
+    delay: 1600
+  });
+}
+  
+document.addEventListener('DOMContentLoaded', function() {
+  var i = 0;
+  var txt = 'From the sea to the highest mountain, we strive towards the same goal. Heed the warning, Adventurer, No preparation is too long. Use every skill you have learned, every experience you have. For no obstacle would come without consequences. Push through, Adventurer, for no matter how dangerous the dungeons are, there is light at the end of it all.';
+  var speed = 50;
+  
+  function typeWriter() {
+    if (i < txt.length) {
+      document.getElementById("descdlor24").innerHTML += txt.charAt(i);
+      i++;
+      setTimeout(typeWriter, speed);
+    } 
+    else {
+      // The typewriter has completed, now display the <h3>
+      setTimeout(function() {
+        document.getElementById("h3-text").classList.add('opacity-100');
+        startAnimation();
+      }, 1000);
+    }
+  }
+  typeWriter();
+});
